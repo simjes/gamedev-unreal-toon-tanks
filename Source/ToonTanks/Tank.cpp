67ae1d512f0805214a1d6ed8e7c03021a15daf0f
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2024-01-25 14:05:59 +0100

    moving and turning

---
+++ Source/ToonTanks/Tank.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+ATank::ATank()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (const APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+			UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(PlayerMappingContext, 0);
+		}
+	}
+}
+
+void ATank::Move(const FInputActionValue& Value)
+{
+	const float Direction = Value.Get<float>();
+	const float DeltaTime = UGameplayStatics::GetWorldDeltaSeconds(this);
+	
+	FVector DeltaLocation = FVector::ZeroVector;
+	DeltaLocation.X = Direction * DeltaTime * Speed;
+	
+	AddActorLocalOffset(DeltaLocation);
+}
+
+void ATank::Turn(const FInputActionValue& Value)
+{
+	const float Direction = Value.Get<float>();
+	const float DeltaTime = UGameplayStatics::GetWorldDeltaSeconds(this);
+
+	FRotator DeltaRotation = FRotator::ZeroRotator;
+	DeltaRotation.Yaw = Direction * DeltaTime * TurnSpeed;
+	AddActorLocalRotation(DeltaRotation);
+}
+
+void ATank::Fire(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Display, TEXT("Firing"));
+}
+
+void ATank::RotateCamera(const FInputActionValue& Value)
+{
+	UE_LOG(LogTemp, Display, TEXT("Rotating"));
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATank::Move);
+		EnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &ATank::Turn);
+		// EnhancedInputComponent->BindAction(RotateAction, ETriggerEvent::Triggered, this, &ATank::RotateCamera);
+		// EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Triggered, this, &ATank::Fire);
+	}
+}
---
+++ Source/ToonTanks/Tank.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tank.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+class UCameraComponent;
+class USpringArmComponent;
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATank();
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* PlayerMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+	void Move(const FInputActionValue& Value);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* TurnAction;
+	void Turn(const FInputActionValue& Value);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* FireAction;
+	void Fire(const FInputActionValue& Value);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* RotateAction;
+	void RotateCamera(const FInputActionValue& Value);
+	
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Pawn Components")
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Pawn Components")
+	UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = "Pawn Components")
+	float Speed = 600;
+
+	UPROPERTY(EditAnywhere, Category = "Pawn Components")
+	float TurnSpeed = 100;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+};
